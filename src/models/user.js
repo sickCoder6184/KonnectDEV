0de0b5a15a4b84f08a5ee5f2953af45cfd884f4d
Preyanshu <preyanshudhapola6184@gmail.com>
2025-08-18 17:57:41 +0530

    Complete Authentication

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 // User Schema
 const userSchema = new mongoose.Schema(
